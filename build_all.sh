1a43a8bb35099dbf4e9cdc40635c36ad178a701e
Oleksii Aliakin <alex@nls.la>
2016-09-08 12:42:45 +0200

    Added web hook notifications to build scripts.
    Signed-off-by: Oleksii Aliakin <alex@nls.la>

--- build_all.sh
+++ build_all.sh
@@ -1,79 +1,12 @@
 #!/bin/bash
 
-DOCKER_EMAIL=${DOCKER_EMAIL:?}
-DOCKER_USERNAME=${DOCKER_USERNAME:?}
-DOCKER_PASSWORD=${DOCKER_PASSWORD:?}
-
 SCRIPT_DIR=$(cd $(dirname $0) && pwd)
 
-images=( "qt" "qbs" "qtcreator" "qttest" "pycharm" "texlive" "traviscli")
-
-function is_the_same_as_latest {
-    local FILE=$1
-    if [ "$FILE" = "Dockerfile" ]; then
-        return 1
-    fi
-    output=$(sha256sum $FILE)
-    hash_current=${output:0:64}
-    output=$(sha256sum Dockerfile)
-    hash_latest=${output:0:64}
-    if [ "$hash_current" = "$hash_latest" ]; then
-        return 0
-    fi
-    return 1
-}
-
-function build {
-    local FILE=$1
-    local NAME=$2
-    local VERSION=$3
-    echo ""
-    echo ""
-    echo "================================================================="
-    echo "Building image $NAME:$VERSION ($FILE)"
-
-    is_the_same_as_latest $FILE
-    if [ $? -eq 0 ]; then
-        echo "$FILE is the same as latest. Just tag and push."
-        docker tag "approximator/$image:latest" "approximator/$image:$VERSION"
-        return $?
-    fi
-
-    echo "docker build -f $FILE -t approximator/$image:$VERSION . &> /tmp/dockerbuild_$image.$VERSION.txt"
-    docker build -f $FILE -t approximator/$image:$VERSION . &> /tmp/dockerbuild_$image.$VERSION.txt
-    return $?
-}
-
-failed=()
-
-for image in "${images[@]}"; do
-    cd $SCRIPT_DIR/$image
-    docker_files=("Dockerfile" $(find . -iname 'Dockerfile_*' -printf '%P\n'))
-    # echo $files
-    for docker_file in "${docker_files[@]}"; do
-        version=${docker_file#*_}
-        if [ "$version" = "Dockerfile" ];then version="latest"; fi
-
-        build $docker_file $image $version
-
-        if [ $? -ne 0 ]; then
-            echo "Error building $image"
-            failed+=("$image:$version")
-            continue
-        fi
-
-        docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
-        docker push "approximator/$image:$version"
-    done
-done
-
-echo ""
-echo ""
-if [ -z "$failed" ]; then
-    echo "Done :)"
-else
-    echo "==================== Errors ===================="
-    for build in "${failed[@]}"; do
-        echo $build
-    done
-fi
+WEBHOOK_URL=https://hooks.microbadger.com/images/approximator/qt/TOslbHvKp5Rk5-rAVIFCRabn1qY=        $SCRIPT_DIR/build.sh qt
+WEBHOOK_URL=https://hooks.microbadger.com/images/approximator/qbs/YAv98VYpn2tF829aRSbGUp3knIs=       $SCRIPT_DIR/build.sh qbs
+WEBHOOK_URL=https://hooks.microbadger.com/images/approximator/qtcreator/TrGtR3Why-98G7-zOaUYNzM_KR8= $SCRIPT_DIR/build.sh qtcreator
+WEBHOOK_URL=https://hooks.microbadger.com/images/approximator/cppcheck/bYLoFZs4A5is__cy6ENxVUd69_k=  $SCRIPT_DIR/build.sh cppcheck
+WEBHOOK_URL=https://hooks.microbadger.com/images/approximator/jupyter/D0oYuf7TdKecFpa08pG-cUrS8x0=   $SCRIPT_DIR/build.sh jupyter
+WEBHOOK_URL=https://hooks.microbadger.com/images/approximator/pycharm/g-6QUo2PiO3EErtM2i_MQ6PztbE=   $SCRIPT_DIR/build.sh pycharm
+WEBHOOK_URL=https://hooks.microbadger.com/images/approximator/qttest/sGTa1LQ5hd_Ef9Z4eoOYrAFfo3I=    $SCRIPT_DIR/build.sh qttest
+WEBHOOK_URL=https://hooks.microbadger.com/images/approximator/traviscli/-EyEPcISuXT3ZKruIVYqk6l4IsA= $SCRIPT_DIR/build.sh traviscli
