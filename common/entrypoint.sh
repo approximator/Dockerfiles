3c4509ddd2bdcad17ed4c5c5199aee5a327261ed
Oleksii Aliakin <alex@nls.la>
2017-04-18 19:05:48 +0200

    [common] Fixed USER

--- common/entrypoint.sh
+++ common/entrypoint.sh
@@ -6,8 +6,9 @@ USER_NAME=${HOST_USER_NAME:-docker_user}
 
 groupadd -g $GROUP_ID $USER_NAME
 useradd -g $USER_NAME -G sudo -N -u $USER_ID $USER_NAME
-chown $USER:$USER /home/$USER
+chown $USER_NAME:$USER_NAME /home/$USER_NAME
 
 export HOME=/home/$USER_NAME
+export USER=$USER_NAME
 
 exec /usr/local/bin/gosu $USER_NAME "$@"
