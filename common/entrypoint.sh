a31b14dd5289ef8a8e6277309eaafc79d8c26f63
Oleksii Aliakin <alex@nls.la>
2017-05-09 20:07:34 +0200

    Entrypoint fix for commands with arguments
    Signed-off-by: Oleksii Aliakin <alex@nls.la>

--- common/entrypoint.sh
+++ common/entrypoint.sh
@@ -4,6 +4,10 @@ USER_ID=${HOST_USER_ID:-1024}
 GROUP_ID=${HOST_USER_GROUP_ID:-$USER_ID}
 USER_NAME=${HOST_USER_NAME:-docker_user}
 
+if [ ! -d /home/$USER_NAME ]; then
+    mkdir -p /home/$USER_NAME
+fi
+
 groupadd -g $GROUP_ID $USER_NAME
 useradd -g $USER_NAME -G sudo -N -u $USER_ID $USER_NAME
 chown $USER_NAME:$USER_NAME /home/$USER_NAME
@@ -11,4 +15,4 @@ chown $USER_NAME:$USER_NAME /home/$USER_NAME
 export HOME=/home/$USER_NAME
 export USER=$USER_NAME
 
-exec /usr/local/bin/gosu $USER_NAME "$@"
+exec /usr/local/bin/gosu $USER_NAME /bin/sh -c "$@"
