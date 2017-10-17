24207b6eb87d3538d20f870e2228b2054d59105f
Oleksii Aliakin <alex@nls.la>
2017-10-17 19:00:56 +0200

    [common] Entrypoint fix.

--- common/entrypoint.sh
+++ common/entrypoint.sh
@@ -4,16 +4,17 @@ USER_ID=${HOST_USER_ID:-1024}
 GROUP_ID=${HOST_USER_GROUP_ID:-$USER_ID}
 USER_NAME=${HOST_USER_NAME:-docker_user}
 
-if [ ! -d /home/$USER_NAME ]; then
-    mkdir -p /home/$USER_NAME
+if [ ! -d "/home/$USER_NAME" ]; then
+    mkdir -p "/home/$USER_NAME"
 fi
 
-grep $GROUP_ID /etc/group >> /dev/null
-[[ $? -eq 0 ]] || groupadd -g $GROUP_ID $USER_NAME
-useradd -N -u $USER_ID $USER_NAME
-chown $USER_ID:$GROUP_ID /home/$USER_NAME
+getent group "$GROUP_ID" >> /dev/null
+[[ $? -eq 0 ]] || groupadd -g "$GROUP_ID" "$USER_NAME"
+useradd -N -u "$USER_ID" "$USER_NAME"
+chown "$USER_ID":"$GROUP_ID" "/home/$USER_NAME"
+chown -R "$USER_ID":"$GROUP_ID" /tmp
 
 export HOME=/home/$USER_NAME
 export USER=$USER_NAME
 
-exec /usr/local/bin/gosu $USER_NAME /bin/sh -c "$@"
+exec /usr/local/bin/gosu "$USER_NAME" /bin/sh -c "$@"
