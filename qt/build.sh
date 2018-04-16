221031015ac042295d40135632ecdb2f767752f2
Approximator <alex@nls.la>
2018-04-16 21:05:28 +0200

    [qt] Added cmake

--- qt/build.sh
+++ qt/build.sh
@@ -7,3 +7,4 @@ FILE=build_Dockerfile_5.10.1
 COMMIT_HASH=`git log -1 --format=%H "$FILE"`
 echo "Building approximator/$image:$VERSION (Commit: $COMMIT_HASH)..."
 docker build --build-arg VCS_REF="$COMMIT_HASH" --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` -f "$FILE" -t "approximator/$image:$VERSION" ..
+docker run -ti --rm -v `pwd`/packages:/out/ --entrypoint bash "approximator/$image:$VERSION" -c "cp -vr /packages/. /out/"
