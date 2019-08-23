ddb9c55f8ca7f5c35d88b2dfc5aac84e9d9a88eb
Approximator <alex@nls.la>
2019-08-23 21:41:04 +0200

    Add qt 5.13.0 built with conan

--- qt/build.sh
+++ qt/build.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 
 image="qt"
-VERSION="build_5.10.1"
+VERSION="conan_build"
 
-FILE=build_Dockerfile_5.10.1
-COMMIT_HASH=`git log -1 --format=%H "$FILE"`
+FILE="Dockerfile_conan_5.13.0"
+COMMIT_HASH=`git log -1 --format=%H ../"$FILE"`
 echo "Building approximator/$image:$VERSION (Commit: $COMMIT_HASH)..."
-docker build --build-arg VCS_REF="$COMMIT_HASH" --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` -f "$FILE" -t "approximator/$image:$VERSION" ..
+
+SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
+
+cd "$SCRIPT_DIR"
+
+docker build  \
+  --build-arg VCS_REF="$COMMIT_HASH" \
+  --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` \
+  -f "$FILE" -t "approximator/$image:$VERSION" .
+
+docker run --rm -ti                     \
+  -e BINTRAY_APIKEY="$BINTRAY_APIKEY"   \
+  -e BUILD_TYPE="$BUILD_TYPE"           \
+  -e LIBCXX="$LIBCXX"                   \
+  "approximator/$image:$VERSION"        \
+  bash -c "conan user -p $BINTRAY_APIKEY -r approximator approximator && conan install /conanfile -s build_type=$BUILD_TYPE --build=missing && conan upload qt/5.13.0@bincrafters/stable --all -r=approximator"
