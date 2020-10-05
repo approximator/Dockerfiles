39199728f9e9a7a2fadc453f182aea366fa05011
Approximator <alex@nls.la>
2020-10-05 20:35:04 +0200

    [qt] Use conan_server and rclone to mount conan data from s3

--- .github/actions/conan/entrypoint.sh
+++ .github/actions/conan/entrypoint.sh
@@ -1,4 +1,16 @@
-#!/bin/bash
+#!/bin/bash -x
 
 env
+rm -rf /github/workspace/.conan/
+ls -lah /github/workspace/
+
+gunicorn -b 0.0.0.0:9300 -w 4 -t 300 conans.server.server_launcher:app &
+sleep 10
+conan remote add bincrafters https://api.bintray.com/conan/bincrafters/public-conan
+conan remote add myremote http://localhost:9300
+conan user -p demo -r myremote demo
+
+conan profile new default --detect
+conan profile update settings.compiler.libcxx=libstdc++11 default
+
 bash -c "$INPUT_CONAN_CMD"
