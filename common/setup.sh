706eff882014e9e6b92b8387abcd94f03a236ab3
Approximator <alex@nls.la>
2021-05-22 16:37:10 +0200

    [common] Install gosu from packages

--- common/setup.sh
+++ common/setup.sh
@@ -3,19 +3,17 @@
 DEBIAN_FRONTEND=noninteractive apt-get update && \
 DEBIAN_FRONTEND=noninteractive apt-get -y --no-install-recommends install \
     ca-certificates \
+    gosu            \
     gpg             \
     libssl-dev      \
     libuv1-dev      \
+    locales         \
     sudo            \
+    tzdata          \
     wget
 
-gpg --keyserver ha.pool.sks-keyservers.net --recv-keys B42F6819007F00F88E364FD4036A9C25BF357DD4
+ln -fs /usr/share/zoneinfo/Europe/Warsaw /etc/localtime
+dpkg-reconfigure -f noninteractive tzdata
 
-arch="$(dpkg --print-architecture | awk -F- '{ print $NF }')"
-cd /usr/local/bin
-wget -O gosu "https://github.com/tianon/gosu/releases/download/1.10/gosu-$arch"
-wget -O gosu.asc "https://github.com/tianon/gosu/releases/download/1.10/gosu-$arch.asc"
-gpg --verify gosu.asc
-rm gosu.asc
-chmod +x gosu
-# ./gosu
+locale-gen en_US.UTF-8
+update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
